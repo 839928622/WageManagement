f47f20fcd96bd56786ebd618de3a229ec088f71d
文科 蒋 <839928622@qq.com>
2019-03-06 10:50:58 +0800

    添加项目文件。

---
+++ WageManagementSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WageManagementSystem.Startup))]
+namespace WageManagementSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
