8288f8f5a93ca73b9967cfdfe322dce7637de0de
文科 蒋 <839928622@qq.com>
2019-03-10 14:39:41 +0800

    Add jquery.datatables plugin

--- WageManagementSystem/App_Start/BundleConfig.cs
+++ WageManagementSystem/App_Start/BundleConfig.cs
@@ -20,7 +20,10 @@ namespace WageManagementSystem
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.js",
+                       "~/Scripts/bootbox.js"//import bootbox.js to bundles
+
+                      ));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
